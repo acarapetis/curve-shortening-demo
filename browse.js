abb31ca6a604ab9507f004d344e03cf6a7eee9f2
Anthony Carapetis <anthony.carapetis@gmail.com>
2019-05-27 14:37:27 +1000

    Fix live-reloading to notice index.html

--- browse.js
+++ browse.js
@@ -3,18 +3,24 @@
 var bs = require("browser-sync").create();
 var fs = require('fs')
 
-fs.readFile('index.html', 'utf8', function (err, data) {
-    var result = data.replace(/bundle.min.mjs/g, 'bundle.mjs');
+function buildIndex() {
+    fs.readFile('index.html', 'utf8', function (err, data) {
+        var result = data.replace(/bundle.min.mjs/g, 'bundle.mjs');
 
-    fs.writeFile('index.dev.html', result, 'utf8', function (err) {
-        if (err) return console.log(err);
+        fs.writeFile('index.dev.html', result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
     });
-});
+}
 
-
+buildIndex();
 bs.init({
     files: [
-        '*.html',
+        {
+            match: ['index.html'],
+            fn: buildIndex,
+        },
+        'index.dev.html',
         'build/*.js',
         'build/*.mjs',
         '*.css',
